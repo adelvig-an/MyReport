6917b46f907816ad500f0dc05bf8a384e1e8f4d6
adelvig-an <63202963+adelvig-an@users.noreply.github.com>
2021-03-30 23:55:58 +0700

    ModalWindow

---
+++ MyReport/ModalWindow/ModalWindowResult.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyReport.ModalWindow
+{
+    /// <summary>
+    /// An enum representing the result of a Message Dialog.
+    /// </summary>
+    public enum ModalWindowResult
+    {
+        Canceled = -1,
+        Negative = 0,
+        Affirmative = 1,
+        FirstAuxiliary,
+        SecondAuxiliary
+    }
+}
