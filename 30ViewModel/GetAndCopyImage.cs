cfca793a651204db7d59a13cd3acd9967fadeea5
Adelvig <adelvig@yandex.ru>
2021-10-30 00:22:31 +0700

    Рефакторинг

--- 30ViewModel/GetAndCopyImage.cs
+++ 30ViewModel/GetAndCopyImage.cs
@@ -8,7 +8,7 @@ namespace _30ViewModel
 {
     public static class GetAndCopyImage
     {
-        public static void AddInsurancePolicieImage()
+        public static string CopyImage()
         {
             OpenFileDialog OpenFileDialog = new OpenFileDialog();
             OpenFileDialog.Filter = "Пользовательские файлы (*.jfif; *.pjpeg; *.jpeg; *.pjp; *.tiff; *.bmp; *.jpg; *.png) |*.jfif; *.pjpeg; *.jpeg; *.pjp; *.tiff; *.bmp; *.jpg; *.png";
@@ -22,58 +22,60 @@ namespace _30ViewModel
                     newFileName = Path.GetRandomFileName() + ".jfif";
                     File.Copy(filePath, newFileName);
                     //PathInsurancePolicieCollection.Add(newFileName);
+                    return newFileName;
                 }
                 else if (extension == ".pjpeg")
                 {
                     newFileName = Path.GetRandomFileName() + ".pjpeg";
                     File.Copy(filePath, newFileName);
                     //PathInsurancePolicieCollection.Add(newFileName);
-
+                    return newFileName;
                 }
                 else if (extension == ".jpeg")
                 {
                     newFileName = Path.GetRandomFileName() + ".jpeg";
                     File.Copy(filePath, newFileName);
                     //PathInsurancePolicieCollection.Add(newFileName);
-
+                    return newFileName;
                 }
                 else if (extension == ".pjp")
                 {
                     newFileName = Path.GetRandomFileName() + ".pjp";
                     File.Copy(filePath, newFileName);
                     //PathInsurancePolicieCollection.Add(newFileName);
-
+                    return newFileName;
                 }
                 else if (extension == ".tiff")
                 {
                     newFileName = Path.GetRandomFileName() + ".tiff";
                     File.Copy(filePath, newFileName);
                     //PathInsurancePolicieCollection.Add(newFileName);
-
+                    return newFileName;
                 }
                 else if (extension == ".bmp")
                 {
                     newFileName = Path.GetRandomFileName() + ".bmp";
                     File.Copy(filePath, newFileName);
                     //PathInsurancePolicieCollection.Add(newFileName);
-
+                    return newFileName;
                 }
                 else if (extension == ".jpg")
                 {
                     newFileName = Path.GetRandomFileName() + ".jpg";
                     File.Copy(filePath, newFileName);
                     //PathInsurancePolicieCollection.Add(newFileName);
-
+                    return newFileName;
                 }
                 else if (extension == ".png")
                 {
                     newFileName = Path.GetRandomFileName() + ".png";
                     File.Copy(filePath, newFileName);
                     //PathInsurancePolicieCollection.Add(newFileName);
+                    return newFileName;
                 }
                 else
                 {
-                    throw new NotImplementedException();
+                    return null;
                 }
             }
         }
