6b940a43a339769e2ec548dcc4582e0cb55ff3e0
Adelvig <adelvig@yandex.ru>
2022-01-05 15:59:05 +0700

    обноление 05.01.22

---
+++ 30ViewModel/MWindow/ViewModel/AppraiserDialogVM.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _30ViewModel.MWindow.ViewModel
+{
+    public class AppraiserDialogVM : MsgViewModel
+    {
+    }
+}
